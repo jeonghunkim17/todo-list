2f74fbb87781c94f632456d35ffc4eeb498086a0
jeonghunkim17 <jhmaster17@gmail.com>
2023-05-19 10:42:30 +1200

    Create randomImg.js

---
+++ js/randomImg.js
@@ -0,0 +1,23 @@
+const images = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+];
+
+const randomImg = images[Math.floor(Math.random() * images.length)];
+
+const figure = document.querySelector(".randomImg figure");
+
+const img = document.createElement("img");
+img.src = `./img/${randomImg}`;
+
+figure.appendChild(img);
